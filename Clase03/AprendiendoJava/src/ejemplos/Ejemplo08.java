69d43e2bc4078e7b3cb772d5307995610ff1dc82
Gustavo Coronel <gcoronelc@gmail.com>
2021-10-17 09:15:49 -0500

    Clase 03

--- Clase03/AprendiendoJava/src/ejemplos/Ejemplo08.java
+++ Clase03/AprendiendoJava/src/ejemplos/Ejemplo08.java
@@ -1,10 +1,11 @@
 package ejemplos;
 
+import java.util.Scanner;
+
 /*
-
-
-*/
-
+Este ejemplo muestra la flexibilidad de la estructura for.
+Calcula el MCD y MCD de dos numeros.
+ */
 /**
  * @author Eric Gustavo Coronel Castillo
  * @blog www.desarrollasoftware.com
@@ -16,10 +17,14 @@ package ejemplos;
 public class Ejemplo08 {
 
 	public static void main(String[] args) {
-
-		int n1 = Integer.parseInt(args[0]);
-		int n2 = Integer.parseInt(args[1]);
-		int n1_aux = n1, n2_aux = n2, mcd, mcm;
+		// Variables
+		int n1, n2, n1_aux, n2_aux, mcd, mcm;
+		// Lectura de datos
+		n1 = leerEnteroPositivo("Ingrese valor de n1: ");
+		n2 = leerEnteroPositivo("Ingrese valor de n2: ");
+		// Proceso
+		n1_aux = n1;
+		n2_aux = n2;
 		for (; n1 != n2;) {
 			if (n1 > n2) {
 				n1 -= n2;
@@ -29,10 +34,24 @@ public class Ejemplo08 {
 		}
 		mcd = n1;
 		mcm = n1_aux * n2_aux / mcd;
+		// Reporte
+		System.out.println("");
 		System.out.println("n1 => " + n1_aux);
 		System.out.println("n2 => " + n2_aux);
 		System.out.println("mcd => " + mcd);
 		System.out.println("mcm => " + mcm);
+	}
+
+	private static int leerEnteroPositivo(String etiqueta) {
+		Scanner scanner = new Scanner(System.in);
+		int n;
+		// Proceso: Inicio
+		do {
+			System.out.print(etiqueta);
+			n = scanner.nextInt();
+		} while (n <= 0);
+		// Proceso: Fin
+		return n;
 	}
 
 }
