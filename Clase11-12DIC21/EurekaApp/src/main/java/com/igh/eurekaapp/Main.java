1014d6402150ec6ed515d29a573ce0907722979b
Gustavo Coronel <gcoronelc@gmail.com>
2021-12-12 11:45:46 -0500

    Clase 11

---
+++ Clase11-12DIC21/EurekaApp/src/main/java/com/igh/eurekaapp/Main.java
@@ -0,0 +1,17 @@
+package com.igh.eurekaapp;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @blog www.desarrollasoftware.com
+ * @email gcoronelc@gmail.com
+ * @youtube www.youtube.com/DesarrollaSoftware
+ * @facebook www.facebook.com/groups/desarrollasoftware
+ * @cursos gcoronelc.github.io
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
