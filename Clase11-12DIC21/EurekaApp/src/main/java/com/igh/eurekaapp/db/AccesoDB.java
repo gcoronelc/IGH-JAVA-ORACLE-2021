bc815b46dca54d328a7b60033e91d62ad87617c4
Gustavo Coronel <gcoronelc@gmail.com>
2021-12-12 13:03:47 -0500

    Clase 11

--- Clase11-12DIC21/EurekaApp/src/main/java/com/igh/eurekaapp/db/AccesoDB.java
+++ Clase11-12DIC21/EurekaApp/src/main/java/com/igh/eurekaapp/db/AccesoDB.java
@@ -20,7 +20,7 @@ public class AccesoDB {
 	public static Connection getConnection() throws SQLException {
 		String driver = "oracle.jdbc.OracleDriver";
 		String urlDB = "jdbc:oracle:thin:@localhost:1521/XE";
-		String user = "pepe";
+		String user = "eureka";
 		String pass = "admin";
 		Connection cn = null;
 		try {
---
+++ Clase11-12DIC21/EurekaApp/src/main/java/com/igh/eurekaapp/prueba/Prueba06.java
@@ -0,0 +1,53 @@
+package com.igh.eurekaapp.prueba;
+
+import com.igh.eurekaapp.db.AccesoDB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @blog www.desarrollasoftware.com
+ * @email gcoronelc@gmail.com
+ * @youtube www.youtube.com/DesarrollaSoftware
+ * @facebook www.facebook.com/groups/desarrollasoftware
+ * @cursos gcoronelc.github.io
+ */
+public class Prueba06 {
+
+	public static void main(String[] args) {
+		String sql = "SELECT CHR_CUENCODIGO,CHR_MONECODIGO,CHR_SUCUCODIGO,CHR_CLIECODIGO, "
+				  + "DEC_CUENSALDO,VCH_CUENESTADO,INT_CUENCONTMOV FROM CUENTA WHERE CHR_SUCUCODIGO=? ";
+		Connection cn = null;
+		PreparedStatement pstm;
+		ResultSet rs = null;
+		int filas;
+		String rowData;
+		try {
+			cn = AccesoDB.getConnection();
+			pstm = cn.prepareStatement(sql);
+			pstm.setString(1, "002");
+			rs = pstm.executeQuery();
+			while (rs.next()) {
+				rowData = "";
+				rowData += rs.getString("CHR_CUENCODIGO") + " - ";
+				rowData += rs.getString("CHR_MONECODIGO") + " - ";
+				rowData += rs.getDouble("DEC_CUENSALDO") + " - ";
+				rowData += rs.getString("VCH_CUENESTADO") + " - ";
+				rowData += rs.getString("INT_CUENCONTMOV");
+				System.out.println(rowData);
+			}
+			rs.close();
+			pstm.close();
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e.getMessage());
+		} finally {
+			try {
+				cn.close();
+			} catch (Exception e) {
+			}
+		}
+	}
+
+}
