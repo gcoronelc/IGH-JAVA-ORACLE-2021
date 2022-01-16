51496c25614eb449b2c26446d904b1414c74704e
Gustavo Coronel <gcoronelc@gmail.com>
2022-01-16 12:57:47 -0500

    Clase 14

---
+++ Clase14-17ENE22/SumaApp/src/main/java/com/igh/sumaapp/service/MatematicaService.java
@@ -0,0 +1,17 @@
+package com.igh.sumaapp.service;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @blog www.desarrollasoftware.com
+ * @email gcoronelc@gmail.com
+ * @youtube www.youtube.com/DesarrollaSoftware
+ * @facebook www.facebook.com/groups/desarrollasoftware
+ * @cursos gcoronelc.github.io
+ */
+public class MatematicaService {
+	
+	public int sumar(int num1, int num2){
+		return (num1 + num2);
+	}
+
+}
---
+++ Clase14-17ENE22/SumaApp/src/main/java/com/igh/sumaapp/servlets/Sumar.java
@@ -0,0 +1,44 @@
+package com.igh.sumaapp.servlets;
+
+import com.igh.sumaapp.service.MatematicaService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+@WebServlet(name = "Sumar", urlPatterns = {"/Sumar"})
+public class Sumar extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Variables
+		int num1, num2, suma;
+		// Datos
+		num1 = Integer.parseInt(request.getParameter("num1"));
+		num2 = Integer.parseInt(request.getParameter("num2"));
+		// Proceso
+		MatematicaService service = new MatematicaService();
+		suma = service.sumar(num1, num2);
+		// Respuesta
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head><title>RESULTADO SOLICITADO</title></head>");
+		out.println("<body>");
+		out.println("<h1>SUMA DE 2 NUMEROS</h1>");
+		out.println("<p>Número 1: " + num1 + "</p>");
+		out.println("<p>Número 2: " + num2 + "</p>");
+		out.println("<p>Suma: " + suma + "</p>");
+		out.println("<a href='index.html'>Retornar</a>");
+		out.println("</body></html>");
+	}
+
+}
